ebf5e7ba504bfb55eaa14682d2d838c34f79c20b
dilip9398 <138967561+dilip9398@users.noreply.github.com>
2024-09-20 21:37:45 +0530

    Update numberguessing.py

--- numberguessing.py
+++ numberguessing.py
@@ -1,12 +1,56 @@
 import random
-guess = random.randint(1,99)
-num = int(input("Enter the number: "))
+print("Welcome to the number guessing game!\n You have got 5 chances \n Guess the Number now")
+print("""
+           ________
+        _jgN########Ngg_
+      _N##N@@""  ""9NN##Np_
+     d###P            N####p
+     "^^"              T####
+                       d###P
+                    _g###@F
+                 _gN##@P
+               gN###F"
+              d###F
+             0###F
+             0###F
+             0###F
+             "NN@'
 
+              ___
+             q###r
+             """)
+def play_game():
+    number_guess = random.randrange(100)
 
-while guess != num:
-    if guess < num:
-        print("Your guess is too low.")
-    elif guess > num :
-        print("Your guess is too high.")
-    guess = int(input("Guess again: "))
-print("Your guess is correct.")
+    chances = 5
+    counter_guess = 0
+
+    while counter_guess <= chances:
+
+        counter_guess += 1
+        guess = input("Guess the Number: ")
+
+        try:
+            guess = int(guess)
+        except ValueError:
+            print("Enter the valid number!")
+            continue
+
+        if guess == number_guess:
+            print("You Guessed the correct number!\n Your IQ levels are Good!")
+            break
+        elif counter_guess == chances and guess != number_guess:
+            print(f"Sorry! Better luck next time!. The Number is {number_guess}")
+        elif guess < number_guess:
+            print("Your guess is lower!")
+        elif guess > number_guess:
+            print("Your guess is Higher!")
+            
+            
+    play_again = input("Do you want to guess again? (Y/N)")
+    if play_again.lower() == "y":
+        play_game()
+    else:
+        print("Thankyou for playing!")
+
+play_game()
